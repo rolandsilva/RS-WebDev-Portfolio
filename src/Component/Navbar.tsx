46d6cdc2c2e92a1756010bcc72555c03abc55fb9
Roland Silva <roland.silva@me.com>
2024-10-14 22:06:12 -0400

    RS WebDev Portfolio add home page and Navbar

---
+++ src/Component/Navbar.tsx
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="h1Home">
+      <ul>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+        <li>Resume</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
